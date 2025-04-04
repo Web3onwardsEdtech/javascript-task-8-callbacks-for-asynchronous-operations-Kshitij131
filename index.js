ab3a2376735e26db3cf301f49493a44b5c73e48c
Kshitij131 <1ms23cy032@msrit.edu>
2025-04-05 02:14:48 +0530

    Completed task 8

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 
+function delayedExecution(callback, delay, ...args) {
+    setTimeout(() => {
+        callback(...args);
+    }, delay);
+}
+
+
+function printMessage(message) {
+    console.log(message);
+}
+delayedExecution(printMessage, 1000, "This is a delayed message.");
+
+
+
+function addNumbers(a, b) {
+    console.log(a + b);
+}
+delayedExecution(addNumbers, 3000, 5, 10);
+
+
+
+function logCurrentTime() {
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    console.log(`Current time: ${hours}:${minutes}`);
+}
+delayedExecution(logCurrentTime, 5000);
+
